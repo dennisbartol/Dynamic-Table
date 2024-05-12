84ec0531f4a2bac7905e754e049fea9c7ae7ed32
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-12 17:37:57 +0200

    Update dynamicTable.js

--- dynamicTable.js
+++ dynamicTable.js
@@ -1,1 +1,19 @@
+const teamMembers = [
+    {
+        src: 'assets/profile.jpg', 
+        name: 'James Alexander', 
+        alias: '@James', 
+        email: 'blabla@example.com', 
+        status: 'Active', 
+        tags: ['Dev.', 'C#'],
+    },
+    {
+        src: 'assets/profile.jpg', 
+        name: 'Den Bar', 
+        alias: '@DB', 
+        email: 'blablaDB@example.com', 
+        status: 'Active', 
+        tags: ['Ft.End.', 'Dev.'],
+    },
 
+]
